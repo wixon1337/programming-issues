a670f35a13a7fb578688f53d469108f528cc12a0
Vilmos Pásztor <pvilmos93@gmail.com>
2019-06-17 14:46:25 +0200

    átrendezés

--- 2d.js
+++ 2d.js
@@ -8,8 +8,8 @@ const generate2d = (n, m) => {
   return arr;
 };
 
-let k = generate2d(3, 3);
-console.log(k);
+// let k = generate2d(3, 3);
+// console.log(k);
 
 const print2d = (arr) => {
   for (let i = 0; i < arr.length; i++) {
@@ -20,4 +20,42 @@ const print2d = (arr) => {
   }
 };
 
-print2d(k);
+// print2d(k);
+
+const fill2Darray = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = Math.floor(Math.random() * 10 + 1);
+    }
+  }
+  return arr;
+};
+
+let tomb = [[4, 5], [2, 1], [9, 0]];
+console.log(fill2Darray(tomb));
+
+const sum2DArray = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      sum += arr[i][j];
+    }
+  }
+  return sum;
+};
+
+console.log(sum2DArray(tomb));
+
+const max2dArray = (arr) => {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      if (max < arr[i][j]) {
+        max = arr[i][j];
+      }
+    }
+  }
+  return max;
+};
+
+console.log(max2dArray(tomb));
