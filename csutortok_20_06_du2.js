75508a500e38cd747349747dddec97c83b5e196e
Vilmos Pásztor <pvilmos93@gmail.com>
2019-06-20 16:46:44 +0200

    add csut du 3 & modify csut du 2

--- csutortok_20_06_du2.js
+++ csutortok_20_06_du2.js
@@ -1,17 +1,41 @@
-let tomb = [0, 0, 0, 0];
+let tomb = [0, 0, 0, 0, 0, 0];
+console.log(tomb);
+// let temp;
 
-const egyMoz = (arr) => {
-  console.log(arr);
+const step = (arr) => {
+  let x = -1;
   for (let i = 0; i < arr.length; i++) {
-    arr[i] = 1;
-    console.log(arr);
-    arr[i] = 0;
+    if (arr[i] === 1) {
+      x = i;
+    }
   }
-  console.log(arr);
+
+  if (x === -1) {
+    arr[0] = 1;
+    return arr;
+  } else if (x === 0) {
+    arr[x] = 0;
+    arr[x + 1] = 1;
+  } else if (x === arr.length - 1) {
+    arr[x] = 0;
+  } else if (x > 0) {
+    // temp = arr[x];
+    arr[x] = 0;
+    arr[x + 1] = 1;
+  }
+
+  return arr;
+};
+
+const walk = (arr) => {
+  for (let i = 0; i < arr.length + 1; i++) {
+    step(tomb);
+    console.log(tomb);
+  }
 };
 
 const main = () => {
-  egyMoz(tomb);
+  walk(tomb);
 };
 
 main();
